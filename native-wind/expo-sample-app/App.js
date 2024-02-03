4956fe378d5848b6cd2216cdfd76e89e43f84adb
Li_Tian <Li_Tian@singaporeair.com.sg>
2024-02-03 14:57:31 +0800

    close #2, make background fullscreen via className [2]

--- native-wind/expo-sample-app/App.js
+++ native-wind/expo-sample-app/App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View className="flex-1 items-center justify-center bg-amber-200" >
+            <Image
+                blurRadius={70}
+                source={require('./assets/bg.png')}
+                className="absolute h-full w-full"/>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+
+
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
